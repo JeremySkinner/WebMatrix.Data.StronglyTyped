3612818ca45d9dc4bd40224d71a6299097fc0f0d
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2011-03-01 18:14:19 +0000

    Build script for publishing to nuget.

---
+++ Build.cmd
@@ -0,0 +1,12 @@
+@echo off
+if "%1"=="PublishPackages" goto publish
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe buildscripts\build.proj %*
+goto end
+
+:publish
+powershell.exe -noprofile buildscripts\publish-nuget-packages.ps1
+
+goto end
+
+:end
